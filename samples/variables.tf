34c8e99a43e104d4b5009c6288da08ffc32f1c13
root <root@ip-172-31-0-4.ap-northeast-1.compute.internal>
2026-01-08 14:44:14 +0000

    add sample .tf files

---
+++ samples/variables.tf
@@ -0,0 +1,10 @@
+variable "owner" {
+  description = "Owner name (student name)"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for VPC"
+  type        = string
+}
+
